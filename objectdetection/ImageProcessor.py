8e9421c04e6866343fffea28ed35f7821d7163b5
tolga <turhantolga.unal@bahcesehir.edu.tr>
2025-03-04 15:37:09 +0300

    Reformatted main and moved the processing logic to a new class inside ImageProcessing.py for making debugging easier

---
+++ objectdetection/ImageProcessor.py
@@ -0,0 +1,168 @@
+import cv2
+from matplotlib import pyplot as plt
+from matplotlib import cm
+from pytesseract import image_to_string
+import numpy as np
+import json
+
+
+class ImageProcessor:
+    def __init__(self, image_path: str):
+        """
+        Loads the image for later processing operations
+
+        Parameters:
+         image_path (str): The name of the image that will be processed
+        """
+
+        # Variables that will be initialized later in other functions
+        self.filtered_contours = None
+        self.min_area = None
+        self.max_area = None
+        self.all_contours = None
+        self.cv2_image_blurred = None
+        self.cv2_image_gray = None
+
+        self.img_path = image_path
+        self.cv2_image = cv2.imread(image_path)
+        self.preprocess_image()
+        self.extract_contours()
+        self.filter_contours()
+        self.buildings = {"Buildings": []}
+
+    def preprocess_image(self):
+        """
+        For use within the init, create a grayscale version of the image and creates another version that
+        has gaussian blur applied
+        """
+        self.cv2_image_gray = cv2.cvtColor(self.cv2_image, cv2.COLOR_BGR2GRAY)  # convert image into grayscale
+        self.cv2_image_blurred = cv2.GaussianBlur(self.cv2_image_gray, (5, 5), 1)  # apply blur to image
+
+    def extract_contours(self):
+        """For use in init, Apply canny edge detection and get all the contours that are found"""
+        canny = cv2.Canny(self.cv2_image_blurred, 10, 50)  # apply canny to roi
+        # Find contours
+        self.all_contours = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+
+    def filter_contours(self):
+        """For use within init, filter contours in order to only get the corner vertices for shape detection
+            also filters the image itself if it is detected as an object"""
+
+        # First, determine max and min areas for thresholding
+        self.max_area = self.cv2_image.shape[0] * self.cv2_image.shape[1] * 0.9
+        self.min_area = 100
+
+        # Second, create a filtered list
+        self.filtered_contours = []
+        for contour in self.all_contours:
+            area = cv2.contourArea(contour)
+            if self.min_area < area < self.max_area:
+                epsilon = 0.04 * cv2.arcLength(contour, True)  # Adjust epsilon as needed. Lower=more detail
+                approx = cv2.approxPolyDP(contour, epsilon, True)
+                self.filtered_contours.append(approx)
+
+    def extract_building_details(self):
+        """Call this function to process the image and prepare the json data"""
+        # for enumerating the building Ids
+        current_id = 1
+
+        for contour in self.filtered_contours:
+            # Create temporary dict that will be added to the list
+            building = {"Id": current_id}
+
+            # Increment it
+            current_id += 1
+
+            # Get the bounding rectangle around the contour
+            x, y, w, h = cv2.boundingRect(contour)
+            building = self.extract_vertex_coordinates(contour, building)
+
+            # Extract the region of interest (ROI) from the original image
+            roi, roi_grayscale = self.extract_roi(x, y, w, h)
+
+            print("Total edges: " + str(len(contour)))
+
+            building["Height"] = self.extract_number(roi_grayscale)
+            # Append the building data to the list
+            self.buildings["Buildings"].append(building)
+
+            # # Show the extracted ROI (optional)
+            # cv2.imshow('ROI', 255 - thresh)
+            # cv2.waitKey(0)
+
+    def extract_vertex_coordinates(self, contour: list, building: dict):
+        """For use withing the extract_building_details, gets the coordinates of the vertices in a group of vertices
+        Parameters:
+            contour (list): The group of vertices
+            building (dict): The dict that will be appended
+        Returns:
+            building (dict): The new building dictionary
+            """
+        for count, vertex in enumerate(contour):
+            building[f"XofV{count}"] = int(vertex[0][0])
+            building[f"YofV{count}"] = int(vertex[0][1])
+        return building
+
+    def extract_roi(self, x, y, w, h):
+        """For use within the extract_building_details, gets the image withing a bounding box
+        Parameters:
+            x: x coordinate
+            y: y coordinate
+            w: width of the bounding box
+            h: height of the bounding box
+        Returns:
+            roi: Region of interest
+            roi_grayscale: Region of interest in grayscale"""
+        # roi = img[y+10:y + h-10, x+10:x + w-10]
+        roi = self.cv2_image[y:y + h, x:x + w]
+        cv2.imshow("ex", roi)
+        roi_grayscale = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        return roi, roi_grayscale
+
+    def extract_number(self, roi_grayscale):
+        """Extracts the number from the region of interest and returns it
+        Parameters:
+            roi_grayscale: Region of interest in grayscale
+        Returns:
+            text_number: The number that is found"""
+        # threshold for easier number detection
+        _, thresh = cv2.threshold(roi_grayscale, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+        # Perform OCR
+        text = image_to_string(thresh,
+                               config='--psm 7 --oem 3 -c tessedit_char_whitelist=0123456789')  # psm 7 for a single line of text
+        inv_text = image_to_string(255 - thresh, config='--psm 7 --oem 3 -c tessedit_char_whitelist=0123456789')
+        text_gray = image_to_string(roi_grayscale,
+                                    config='--psm 7 --oem 3 -c tessedit_char_whitelist=0123456789')  # psm 7 for a single line of text
+        inv_text_gray = image_to_string(255 - roi_grayscale,
+                                        config='--psm 7 --oem 3 -c tessedit_char_whitelist=0123456789')
+
+        text_number = -1
+
+        if text.strip():
+            print(f"Detected number: {text.strip()}")
+            text_number = text.strip()
+        elif inv_text.strip():
+            print(f"Detected number: {inv_text.strip()}")
+            text_number = inv_text.strip()
+        elif text_gray.strip():
+            print(f"Detected number: {text_gray.strip()}")
+            text_number = text_gray.strip()
+        elif inv_text_gray.strip():
+            print(f"Detected number: {inv_text_gray.strip()}")
+            text_number = inv_text_gray.strip()
+        else:
+            print("Couldn't find number.")
+        return text_number
+
+    def show_final(self):
+        """Creates the environment
+        of the picture and shows it"""
+        cv2.drawContours(self.cv2_image, self.filtered_contours, -1, (0, 255, 0), 3)
+        plt.subplot(1, 1, 1)
+        plt.imshow(self.cv2_image, cmap=cm.bone)
+        plt.show()
+
+    def export_json(self):
+        with open('result.json', 'w') as fp:
+            json.dump(self.buildings, fp)
