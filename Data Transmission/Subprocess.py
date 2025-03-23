b3e9b5cff1fad1d5b0dafc45b6236c5036e123cb
yaseenyousif <145763667+yaseenyousif@users.noreply.github.com>
2025-03-23 19:15:36 +0300

    Create Subprocess.py

---
+++ Data Transmission/Subprocess.py
@@ -0,0 +1,12 @@
+import subprocess
+
+# Define the command to run the script
+command = ['python',C:\Program Files\Data Transmission_script.py']
+
+# Create a subprocess to run the script
+try:
+    result = subprocess.run(command, check=True, text=True, capture_output=True)
+    print("Output:", result.stdout)  # Output from the script
+    print("Error (if any):", result.stderr)  # Error message (if any)
+except subprocess.CalledProcessError as e:
+    print(f"Error occurred while running the script: {e}")
