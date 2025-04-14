d8e9be766a22b8b177fb4434686bf5e12f2bbeb7
Görkem <revasqueztte@gmail.com>
2025-04-14 19:51:26 +0300

    Changed command path

--- objectdetection/main.py
+++ objectdetection/main.py
@@ -2,7 +2,7 @@ import ImageProcessor as ip
 import subprocess
 
 blender_executable = "C:\\Program Files\\Blender Foundation\\Blender 4.3\\blender.exe"
-script_path = "C:\\Users\\revas\\Repos\\Capstone-Constructor-Robot\\Capstone-Constructor-Robot\\3DVisualization\\GORKEM.py"
+script_path = "C:\\Users\\revas\\Repos\\Capstone-Constructor-Robot\\Capstone-Constructor-Robot\\3DVisualization\\Visualization.py"
 command = [blender_executable, "--background", "--python", script_path]
 
 processor = ip.ImageProcessor("objectdetection/img.png")
